12ef57c47b992fa72c411e183f49a3fdfe527669
GambrinusUp <kotanffas@gmail.com>
2025-02-16 03:23:03 +0700

    add docker config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,4 +4,14 @@ import { defineConfig } from 'vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: 8080,
+    strictPort: true,
+  },
+  server: {
+    port: 8080,
+    strictPort: true,
+    host: true,
+    origin: 'http://localhost:8080',
+  },
 });
--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 const SECRET_KEY = "secret_key";
 const users = [];
